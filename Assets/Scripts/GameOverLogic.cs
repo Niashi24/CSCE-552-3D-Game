be35d3770b03e5b9169ba73ccf958d6999bf717b
Cayden Scruggs <garterscales619@yahoo.com>
2024-12-06 13:10:57 -0500

    init

---
+++ Assets/Scripts/GameOverLogic.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Csce552
+{
+    public class GameOverLogic : MonoBehaviour
+    {
+        public void Restart()
+        {
+            AudioListener.pause = false;
+            Time.timeScale = 1;
+            enabled = true;
+            SceneManager.LoadScene("GameScene");
+        }
+        public void RestartCheckpoint() {
+            AudioListener.pause = false;
+            Time.timeScale = 1;
+            enabled = true;
+            SceneManager.LoadScene("GameScene");
+        }
+        public void ToMainMenu()
+        {
+            AudioListener.pause = false;
+            Time.timeScale = 1;
+            enabled = true;
+            SceneManager.LoadScene("MenuScene");
+        }
+    }
+}
